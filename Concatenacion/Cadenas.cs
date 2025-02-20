66af5be7a316ebc279775d0cbedf5477dd90f3e3
JFVC-98 <jfvcoding@outlook.com>
2025-02-20 15:35:02 -0600

    Se añade CW para obtener el tipo de metodo

--- Concatenacion/Cadenas.cs
+++ Concatenacion/Cadenas.cs
@@ -7,26 +7,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Dictionary<int,object> lista = new Dictionary<int,object>();
-        lista.Add(1, new Clase_1());
-        lista.Add(2, new Clase_2());
+        List<object> lista = new List<object>();
+        lista.Add(new Clase_1());
+        lista.Add(new Clase_2());
         foreach (var item in lista)
         {
-            
+
             //item.Value.GetType().GetMethod("Escoge").Invoke(item.Value, new object[] { 1 });
             //Console.WriteLine(item.Value.GetType().GetMethods(BindingFlags.Public));
-            MethodInfo[] info = item.Value.GetType().GetMethods();
+            MethodInfo[] info = item.GetType().GetMethods();
             foreach (var method in info)
             {
                 if (method.ReturnType == typeof(void))
                 {
                     Console.WriteLine(method.Name);
                     //method.Invoke(item.Value, new object[] { 1 });
-                    if (method.Name!= "Imprimir") { 
+                    /*if (method.Name != "Imprimir")
+                    {
                         //item.Value.GetType().GetMethod(method.Name).Invoke(item.Value, new object[] { 1 });
-                        method.Invoke(item.Value, new object[] { 1 });
-                    }
-                    
+                        method.Invoke(item,new object[] { Convert.ToInt32(Console.ReadLine()) });
+                    }*/
+                    Console.WriteLine(method.DeclaringType);
+
                 }
             }
             /*if (Console.ReadLine()=="1")
